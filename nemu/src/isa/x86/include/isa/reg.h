4af9cf9e8d71dc8e3d5a664ddc085baec6834a02
tracer-ics2019 <tracer@njuics.org>
2023-02-22 01:08:42 -0800

    >  compile 2010234 yuwensq Linux ubuntu 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:56:22 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:08:42 up  2:22,  1 user,  load average: 0.26, 0.20, 0.15 27e11f1c974f16c18087b246938acf66e0023f57

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -18,9 +18,15 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
 typedef struct {
   struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
+	// 匿名联合 https://www.cnblogs.com/guozqzzu/p/3626893.html
+	union {
+		uint32_t _32;
+		uint16_t _16;
+		uint8_t _8[2];
+	};	
+    //uint32_t _32;
+    //uint16_t _16;
+    //uint8_t _8[2];
   } gpr[8];
 
   /* Do NOT change the order of the GPRs' definitions. */
