fd0977f81d576688f23ff0adaa69d4270ecd5ce0
tracer-ics2019 <tracer@njuics.org>
2023-03-05 23:40:34 -0800

    >  compile 2010234 yuwensq Linux ubuntu 5.4.0-144-generic #161~18.04.1-Ubuntu SMP Fri Feb 10 15:55:22 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:40:34 up  1:46,  0 users,  load average: 0.05, 0.05, 0.01 8c64d6edf1a8bb7944972135d2606b1a6801e3cb

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -75,10 +75,9 @@ static bool make_token(char *e) {
 
   nr_token = 0;
 
+  Assert(e, "要解析的字符串为空");
 
-  printf("%sab\n", e);
   while (e[position] != '\0') {
-  printf("%sab\n", e);
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
