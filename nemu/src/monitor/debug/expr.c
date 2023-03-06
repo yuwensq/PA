1123fd8bce2d25e391cc98c60f60e9aba5f3e492
tracer-ics2019 <tracer@njuics.org>
2023-03-06 03:26:52 -0800

    >  compile 2010234 yuwensq Linux ubuntu 5.4.0-144-generic #161~18.04.1-Ubuntu SMP Fri Feb 10 15:55:22 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:26:52 up 18 min,  0 users,  load average: 0.01, 0.07, 0.08 3175c202884a1a724ca32cffa38ad85ce3ec4f6f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -253,5 +253,6 @@ uint32_t expr(char *e, bool *success)
   /* TODO: Insert codes to evaluate the expression. */
   uint32_t result = evalute(nr_token, success);
   nr_token = 0;
+  printf("%d\n", result);
   return result;
 }
