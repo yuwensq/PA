ebec5fb4451e739d0ddcb7fac2d2ce8fa8c3fb52
tracer-ics2019 <tracer@njuics.org>
2023-04-07 00:11:00 -0700

    >  compile 2010234 yuwensq Linux ubuntu 5.4.0-146-generic #163~18.04.1-Ubuntu SMP Mon Mar 20 15:02:59 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:11:00 up  2:51,  0 users,  load average: 0.01, 0.02, 0.00 59907d8f32d6f4c4672559ebf2f0f76da4879138

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -47,16 +47,11 @@ make_EHelper(cmp)
   // *****
   rtl_sub(&s0, &id_dest->val, &id_src->val);
 
-  if (id_dest->width != 4)
-  {
-    rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
-  }
-
   rtl_update_ZFSF(&s0, id_dest->width);
-  rtl_is_sub_carry(&s1, &s0, &id_dest->val);
-  rtl_set_CF(&s1);
   rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s1);
+  rtl_is_sub_carry(&s1, &s0, &id_dest->val);
+  rtl_set_CF(&s1);
 
   print_asm_template2(cmp);
 }
@@ -67,8 +62,6 @@ make_EHelper(inc)
   rtl_li(&s0, 1);
   rtl_add(&s1, &id_dest->val, &s0);
   operand_write(id_dest, &s1);
-
-  Log("%d", s1);
 
   // 001
   rtl_update_ZFSF(&s1, id_dest->width);
