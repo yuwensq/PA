8e51e867a1d13d821c7ee34e1f77c8037d88537b
tracer-ics2019 <tracer@njuics.org>
2023-04-07 00:18:35 -0700

    >  compile 2010234 yuwensq Linux ubuntu 5.4.0-146-generic #163~18.04.1-Ubuntu SMP Mon Mar 20 15:02:59 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:18:35 up  2:59,  0 users,  load average: 0.00, 0.00, 0.00 5b14a525e6fb48c2cf03a59c34f2b82bed8a60

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -46,8 +46,9 @@ make_EHelper(cmp)
   // TODO();
   // *****
   rtl_sub(&s0, &id_dest->val, &id_src->val);
-  Log("1");
+
   rtl_update_ZFSF(&s0, id_dest->width);
+  Log("%d", cpu.ZF);
   rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s1);
   rtl_is_sub_carry(&s1, &s0, &id_dest->val);
