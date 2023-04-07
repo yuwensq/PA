88bf1f952adc6665bd2a7af45414855ed19c27ee
tracer-ics2019 <tracer@njuics.org>
2023-04-07 00:17:57 -0700

    >  compile 2010234 yuwensq Linux ubuntu 5.4.0-146-generic #163~18.04.1-Ubuntu SMP Mon Mar 20 15:02:59 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:17:57 up  2:58,  0 users,  load average: 0.00, 0.00, 0.00 15ceff3dbe0b86729a8c39aea679169dc720552a

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -14,7 +14,7 @@ make_EHelper(jcc) {
   uint32_t cc = decinfo.opcode & 0xf;
   rtl_setcc(&s0, cc);
   if (cc == 5)
-    Log("%d", cpu.CF);
+    Log("%d", cpu.ZF);
   decinfo.is_jmp = s0;
 
   rtl_li(&s1, 0);
