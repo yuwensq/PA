3f697b37c3bfff65e190eace62ea6eaee125c295
tracer-ics2019 <tracer@njuics.org>
2023-04-07 20:37:55 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-146-generic #163~18.04.1-Ubuntu SMP Mon Mar 20 15:02:59 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:37:55 up  1:05,  0 users,  load average: 0.16, 0.09, 0.03 c7344fe85ef0f357184dcc5c5e7f29bfa2804d0f

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -2,46 +2,63 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
   return 0;
 }
 
-char *strcpy(char* dst,const char* src) {
+char *strcpy(char *dst, const char *src)
+{
   return NULL;
 }
 
-char* strncpy(char* dst, const char* src, size_t n) {
+char *strncpy(char *dst, const char *src, size_t n)
+{
   return NULL;
 }
 
-char* strcat(char* dst, const char* src) {
+char *strcat(char *dst, const char *src)
+{
   return NULL;
 }
 
-int strcmp(const char* s1, const char* s2) {
+int strcmp(const char *s1, const char *s2)
+{
+  while (*s1 && *s2 && *s1 == *s2)
+  {
+    s1++;
+    s2++;
+  }
+  if (*s1 - *s2 > 0)
+    return 1;
+  else if (*s1 - *s2 < 0)
+    return -1;
   return 0;
 }
 
-int strncmp(const char* s1, const char* s2, size_t n) {
+int strncmp(const char *s1, const char *s2, size_t n)
+{
   return 0;
 }
 
-void* memset(void* v,int c,size_t n) {
-  char* mem = v;
+void *memset(void *v, int c, size_t n)
+{
+  uint8_t *mem;
   uint8_t byte = (c & 0xff);
-  int i = 0;
-  while (i < n) {
-    mem[i] = byte;
-    i++;
+  for (mem = v; 0 < n; ++mem, --n)
+  {
+    *mem = byte;
   }
   return v;
 }
 
-void* memcpy(void* out, const void* in, size_t n) {
+void *memcpy(void *out, const void *in, size_t n)
+{
   return NULL;
 }
 
-int memcmp(const void* s1, const void* s2, size_t n){
+int memcmp(const void *s1, const void *s2, size_t n)
+{
   return 0;
 }
 
