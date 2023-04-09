d93b387c8d6bb38324dda774473a1a4dd483dbba
tracer-ics2019 <tracer@njuics.org>
2023-04-09 02:46:12 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-146-generic #163~18.04.1-Ubuntu SMP Mon Mar 20 15:02:59 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:46:12 up  3:12,  0 users,  load average: 0.23, 0.11, 0.03 10b460c76410b4bf249a746c433a5e8af168f875

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,19 +3,66 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+int printf(const char *fmt, ...)
+{
   return 0;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
+  int len, i;
+  unsigned long long num;
+  char *str;
+  const char *s;
+
+  for (str = out; *fmt; fmt++)
+  {
+    if (*fmt != '%')
+    {
+      *str++ = *fmt;
+      continue;
+    }
+    fmt++;
+    switch (*fmt)
+    {
+    case 's':
+      s = va_arg(ap, char *);
+      if (!s)
+        s = "<NULL>";
+      len = strlen(s);
+      for (i = 0; i < len; i++)
+      {
+        *str++ = *s++;
+      }
+      continue;
+    case 'd': // 十进制
+      num = va_arg(ap, int32_t);
+      while (num)
+      {
+        *str++ = '0' + (num % 10);
+        num /= 10;
+      }
+      continue;
+    default:
+      break;
+    }
+  }
+  *str = '\0';
+  return str - out;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list args;
+  int i;
+  va_start(args, fmt);
+  i = vsprintf(out, fmt, args);
+  va_end(args);
+  return i;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
   return 0;
 }
 
