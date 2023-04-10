3ced00825cce40408c9eff6b5c1492717550b6af
tracer-ics2019 <tracer@njuics.org>
2023-04-10 01:49:22 -0700

    >  compile 2010234 yuwensq Linux ubuntu 5.4.0-146-generic #163~18.04.1-Ubuntu SMP Mon Mar 20 15:02:59 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:49:22 up 53 min,  0 users,  load average: 0.00, 0.05, 0.01 bd36175dc062e324d6da8c7339dd758dee856b50

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -1,16 +1,29 @@
 #include "nemu.h"
 #include "monitor/diff-test.h"
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if (ref_r->eax != cpu.eax || ref_r->ecx != cpu.ecx || \
-      ref_r->edx != cpu.edx || ref_r->ebx != cpu.ebx || \
-      ref_r->esp != cpu.esp || ref_r->ebp != cpu.ebp || \
-      ref_r->esi != cpu.esi || ref_r->edi != cpu.edi || \
-      ref_r->pc != cpu.pc) {
-        return false;
-      }
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
+{
+  if (ref_r->eax != cpu.eax || ref_r->ecx != cpu.ecx ||
+      ref_r->edx != cpu.edx || ref_r->ebx != cpu.ebx ||
+      ref_r->esp != cpu.esp || ref_r->ebp != cpu.ebp ||
+      ref_r->esi != cpu.esi || ref_r->edi != cpu.edi ||
+      ref_r->pc != cpu.pc)
+  {
+    printf("at pc:0x%08x\n", pc);
+    printf("ref_eax: 0x%08x\n", ref_r->eax);
+    printf("ref_ecx: 0x%08x\n", ref_r->ecx);
+    printf("ref_edx: 0x%08x\n", ref_r->edx);
+    printf("ref_ebx: 0x%08x\n", ref_r->ebx);
+    printf("ref_esp: 0x%08x\n", ref_r->esp);
+    printf("ref_ebp: 0x%08x\n", ref_r->ebp);
+    printf("ref_esi: 0x%08x\n", ref_r->esi);
+    printf("ref_edi: 0x%08x\n", ref_r->edi);
+    printf("ref_eip: 0x%08x\n", ref_r->pc);
+    return false;
+  }
   return true;
 }
 
-void isa_difftest_attach(void) {
+void isa_difftest_attach(void)
+{
 }
