b6046c6aa4e30e1231a959cfe73341b6b5a65395
tracer-ics2019 <tracer@njuics.org>
2023-04-13 03:59:51 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-146-generic #163~18.04.1-Ubuntu SMP Mon Mar 20 15:02:59 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:59:51 up 10:06,  1 user,  load average: 0.82, 0.69, 0.35 a77ed75fce4ce47ed3b8891ec9f9edcc7e8f4e67

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -28,19 +28,31 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size)
   case _DEVREG_VIDEO_FBCTL:
   {
     _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+      uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
 
-    uint32_t *vga_buffer = (uint32_t *)(uintptr_t)FB_ADDR;
-    int bytes_per_row  = sizeof(uint32_t) * (ctl->x + ctl->w > W ? W - ctl->x : ctl->w);
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * (w<W-x?w:W-x);
+      for (int j = 0; j < h && y + j < H; j ++) {
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
 
-    for (int j = 0; j < ctl->h && ctl->y + j < H; j++)
-    {
-      memcpy(vga_buffer + (ctl->y + j) * W + ctl->x, ctl->pixels + ctl->w * j, bytes_per_row);
-    }
+      if (ctl->sync) {
+        outl(SYNC_ADDR, 0);
+      }
+    // uint32_t *vga_buffer = (uint32_t *)(uintptr_t)FB_ADDR;
+    // int bytes_per_row  = sizeof(uint32_t) * (ctl->x + ctl->w > W ? W - ctl->x : ctl->w);
 
-    if (ctl->sync)
-    {
-      outl(SYNC_ADDR, 0);
-    }
+    // for (int j = 0; j < ctl->h && ctl->y + j < H; j++)
+    // {
+    //   memcpy(vga_buffer + (ctl->y + j) * W + ctl->x, ctl->pixels + ctl->w * j, bytes_per_row);
+    // }
+
+    // if (ctl->sync)
+    // {
+    //   outl(SYNC_ADDR, 0);
+    // }
     return size;
   }
   }
