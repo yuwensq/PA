1744833d8c2bffa2fab6fcad658c5b8bb9550892
tracer-ics2019 <tracer@njuics.org>
2023-05-02 20:06:20 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:06:20 up  1:04,  0 users,  load average: 0.06, 0.03, 0.01 7004f35c684c99416c12795fcd35a979241dd688

--- nexus-am/am/am.h
+++ nexus-am/am/am.h
@@ -11,86 +11,92 @@
 #include ARCH_H // "arch/x86-qemu.h", "arch/native.h", ...
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-// ===================== Constants and Structs =======================
+  // ===================== Constants and Structs =======================
 
-enum {
-  _EVENT_NULL = 0,
-  _EVENT_ERROR,
-  _EVENT_IRQ_TIMER,
-  _EVENT_IRQ_IODEV,
-  _EVENT_PAGEFAULT,
-  _EVENT_YIELD,
-  _EVENT_SYSCALL,
-};
+  enum
+  {
+    _EVENT_NULL = 0,
+    _EVENT_ERROR,
+    _EVENT_IRQ_TIMER,
+    _EVENT_IRQ_IODEV,
+    _EVENT_PAGEFAULT,
+    _EVENT_YIELD,
+    _EVENT_SYSCALL,
+  };
 
-enum {
-  _PROT_NONE  = 1, // no access
-  _PROT_READ  = 2, // can read
-  _PROT_WRITE = 4, // can write
-  _PROT_EXEC  = 8, // can execute
-};
+  enum
+  {
+    _PROT_NONE = 1,  // no access
+    _PROT_READ = 2,  // can read
+    _PROT_WRITE = 4, // can write
+    _PROT_EXEC = 8,  // can execute
+  };
 
-// Memory area for [@start, @end)
-typedef struct _Area {
-  void *start, *end;
-} _Area; 
+  // Memory area for [@start, @end)
+  typedef struct _Area
+  {
+    void *start, *end;
+  } _Area;
 
-// An event of type @event, caused by @cause of pointer @ref
-typedef struct _Event {
-  int event;
-  uintptr_t cause, ref;
-  const char *msg;
-} _Event;
+  // An event of type @event, caused by @cause of pointer @ref
+  typedef struct _Event
+  {
+    int event;
+    uintptr_t cause, ref;
+    const char *msg;
+  } _Event;
 
-// Arch-dependent processor context
-typedef struct _Context _Context;
+  // Arch-dependent processor context
+  typedef struct _Context _Context;
 
-// A protected address space with user memory @area
-// and arch-dependent @ptr
-typedef struct _AddressSpace {
-  size_t pgsize;
-  _Area area;
-  void *ptr;
-} _AddressSpace;
+  // A protected address space with user memory @area
+  // and arch-dependent @ptr
+  typedef struct _AddressSpace
+  {
+    size_t pgsize;
+    _Area area;
+    void *ptr;
+  } _AddressSpace;
 
-// ====================== Turing Machine (TRM) =======================
+  // ====================== Turing Machine (TRM) =======================
 
-extern _Area _heap;
-void _putc(char ch);
-void _halt(int code) __attribute__((__noreturn__));
+  extern _Area _heap;
+  void _putc(char ch);
+  void _halt(int code) __attribute__((__noreturn__));
 
-// ======================= I/O Extension (IOE) =======================
+  // ======================= I/O Extension (IOE) =======================
 
-int _ioe_init();
-size_t _io_read(uint32_t dev, uintptr_t reg, void *buf, size_t size);
-size_t _io_write(uint32_t dev, uintptr_t reg, void *buf, size_t size);
+  int _ioe_init();
+  size_t _io_read(uint32_t dev, uintptr_t reg, void *buf, size_t size);
+  size_t _io_write(uint32_t dev, uintptr_t reg, void *buf, size_t size);
 
-// ====================== Context Extension (CTE) ====================
+  // ====================== Context Extension (CTE) ====================
 
-int _cte_init(_Context *(*handler)(_Event ev, _Context *ctx));
-void _yield();
-int _intr_read();
-void _intr_write(int enable);
-_Context *_kcontext(_Area kstack, void (*entry)(void *), void *arg);
+  int _cte_init(_Context *(*handler)(_Event ev, _Context *ctx));
+  void _yield();
+  int _intr_read();
+  void _intr_write(int enable);
+  _Context *_kcontext(_Area kstack, void (*entry)(void *), void *arg);
 
-// ================= Virtual Memory Extension (VME) ==================
+  // ================= Virtual Memory Extension (VME) ==================
 
-int _vme_init(void *(*pgalloc)(size_t size), void (*pgfree)(void *));
-int _protect(_AddressSpace *as);
-void _unprotect(_AddressSpace *as);
-int _map(_AddressSpace *as, void *va, void *pa, int prot);
-_Context *_ucontext(_AddressSpace *as, _Area ustack, _Area kstack,
-                                 void *entry, void *args);
+  int _vme_init(void *(*pgalloc)(size_t size), void (*pgfree)(void *));
+  int _protect(_AddressSpace *as);
+  void _unprotect(_AddressSpace *as);
+  int _map(_AddressSpace *as, void *va, void *pa, int prot);
+  _Context *_ucontext(_AddressSpace *as, _Area ustack, _Area kstack,
+                      void *entry, void *args);
 
-// ================= Multi-Processor Extension (MPE) =================
+  // ================= Multi-Processor Extension (MPE) =================
 
-int _mpe_init(void (*entry)());
-int _ncpu();
-int _cpu();
-intptr_t _atomic_xchg(volatile intptr_t *addr, intptr_t newval);
+  int _mpe_init(void (*entry)());
+  int _ncpu();
+  int _cpu();
+  intptr_t _atomic_xchg(volatile intptr_t *addr, intptr_t newval);
 
 #ifdef __cplusplus
 }
