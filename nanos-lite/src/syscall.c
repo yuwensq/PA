2add0dd54aa11a0471fae5818a716d85c2e16208
tracer-ics2019 <tracer@njuics.org>
2023-05-05 02:46:55 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:46:55 up  1:29,  0 users,  load average: 0.09, 0.17, 0.08 98286c9ef385f89e5f9668b3e483e7b2b3c940

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,7 +20,7 @@ _Context *do_syscall(_Context *c)
     _halt(a[1]);
     break;
   case SYS_write:
-    panic("");
+    c->GPRx = a[3];
   break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
