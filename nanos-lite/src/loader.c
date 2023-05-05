2e30a1e015238200d19cb8c6e077253cfe3772cb
tracer-ics2019 <tracer@njuics.org>
2023-05-04 20:16:07 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:16:07 up 14:06,  0 users,  load average: 0.18, 0.18, 0.15 f583d0439fe29cc63e923c8a61ebf42152076e44

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,25 +2,53 @@
 #include <elf.h>
 
 #ifdef __ISA_AM_NATIVE__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+size_t ramdisk_read(void *, size_t, size_t);
+
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+  // TODO();
+  unsigned char buf[2048];
+  Elf_Ehdr elf_head;
+  ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
+  Elf_Phdr elf_phentry;
+  for (int i = elf_head.e_phoff; i < elf_head.e_phnum * sizeof(Elf_Phdr); i += sizeof(Elf_Phdr))
+  {
+    ramdisk_read(&elf_phentry, i, sizeof(Elf_Phdr));
+    if (elf_phentry.p_type == PT_LOAD)
+    {
+      int len = 0;
+      size_t file_off = elf_phentry.p_offset;
+      unsigned char *mem_addr = (unsigned char*)elf_phentry.p_vaddr;
+      while (len < elf_phentry.p_filesz)
+      {
+        int mov_size = (elf_phentry.p_filesz - len > 2048 ? 2048 : elf_phentry.p_filesz - len);
+        ramdisk_read(buf, file_off, mov_size);
+        memcpy(mem_addr, buf, mov_size);
+        file_off += mov_size;
+        mem_addr += mov_size;
+      }
+      memset(mem_addr, 0, elf_phentry.p_memsz - len);
+    }
+  }
+  return elf_head.e_entry;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB *pcb, const char *filename)
+{
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %x", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
 
-void context_kload(PCB *pcb, void *entry) {
+void context_kload(PCB *pcb, void *entry)
+{
   _Area stack;
   stack.start = pcb->stack;
   stack.end = stack.start + sizeof(pcb->stack);
@@ -28,7 +56,8 @@ void context_kload(PCB *pcb, void *entry) {
   pcb->cp = _kcontext(stack, entry, NULL);
 }
 
-void context_uload(PCB *pcb, const char *filename) {
+void context_uload(PCB *pcb, const char *filename)
+{
   uintptr_t entry = loader(pcb, filename);
 
   _Area stack;
