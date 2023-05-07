37856adebe8195ce1406f57e9020620901070f38
tracer-ics2019 <tracer@njuics.org>
2023-05-07 00:36:30 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:36:30 up 18:52,  0 users,  load average: 0.36, 0.35, 0.23 6e88f5dee52d4d99236de8aa1a74147bee76095a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,39 +16,57 @@ extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
-  // TODO();
-  int fd = fs_open(filename, 0, 0);
-  unsigned char buf[2048];
-  Elf_Ehdr elf_head;
-  fs_read(fd, &elf_head, sizeof(Elf_Ehdr));
-  // ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
-  Elf_Phdr elf_phentry;
-  for (int i = 0; i < elf_head.e_phnum; i++)
-  {
-    fs_lseek(fd, elf_head.e_phoff + i * elf_head.e_phentsize, SEEK_SET);
-    fs_read(fd, &elf_phentry, elf_head.e_phentsize);
-    // ramdisk_read(&elf_phentry, i, sizeof(Elf_Phdr));
-    if (elf_phentry.p_type == PT_LOAD)
-    {
-      int len = 0;
-      size_t file_off = elf_phentry.p_offset;
-      unsigned char *mem_addr = (unsigned char *)elf_phentry.p_vaddr;
-      while (len < elf_phentry.p_filesz)
-      {
-        int mov_size = (elf_phentry.p_filesz - len > 2048 ? 2048 : elf_phentry.p_filesz - len);
-        fs_lseek(fd, file_off, SEEK_SET);
-        fs_read(fd, buf, mov_size);
-        // ramdisk_read(buf, file_off, mov_size);
-        memcpy(mem_addr, buf, mov_size);
-        file_off += mov_size;
-        mem_addr += mov_size;
-        len += mov_size;
-      }
-      memset(mem_addr, 0, elf_phentry.p_memsz - len);
+  //TODO();
+  int fd=fs_open(filename,0,0);
+  printf("%s\n",filename);
+  assert(fd!=-1);
+  Elf_Ehdr elf;
+  fs_read(fd,(void*)&elf,sizeof(Elf_Ehdr));
+  Elf_Phdr phdr[elf.e_phnum];
+  for(size_t i=0;i<elf.e_phnum;i++){
+    fs_lseek(fd,elf.e_phoff+i*elf.e_phentsize,SEEK_SET);
+    fs_read(fd,(void*)&phdr[i],elf.e_phentsize);
+    if(phdr[i].p_type==PT_LOAD){
+      fs_lseek(fd,phdr[i].p_offset,SEEK_SET);
+      fs_read(fd,(void*)phdr[i].p_vaddr,phdr[i].p_filesz);
+      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz),0,phdr[i].p_memsz-phdr[i].p_filesz);
     }
   }
   fs_close(fd);
-  return elf_head.e_entry;
+  return elf.e_entry;
+  // TODO();
+  // int fd = fs_open(filename, 0, 0);
+  // unsigned char buf[2048];
+  // Elf_Ehdr elf_head;
+  // fs_read(fd, &elf_head, sizeof(Elf_Ehdr));
+  // // ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
+  // Elf_Phdr elf_phentry;
+  // for (int i = 0; i < elf_head.e_phnum; i++)
+  // {
+  //   fs_lseek(fd, elf_head.e_phoff + i * elf_head.e_phentsize, SEEK_SET);
+  //   fs_read(fd, &elf_phentry, elf_head.e_phentsize);
+  //   // ramdisk_read(&elf_phentry, i, sizeof(Elf_Phdr));
+  //   if (elf_phentry.p_type == PT_LOAD)
+  //   {
+  //     int len = 0;
+  //     size_t file_off = elf_phentry.p_offset;
+  //     unsigned char *mem_addr = (unsigned char *)elf_phentry.p_vaddr;
+  //     while (len < elf_phentry.p_filesz)
+  //     {
+  //       int mov_size = (elf_phentry.p_filesz - len > 2048 ? 2048 : elf_phentry.p_filesz - len);
+  //       fs_lseek(fd, file_off, SEEK_SET);
+  //       fs_read(fd, buf, mov_size);
+  //       // ramdisk_read(buf, file_off, mov_size);
+  //       memcpy(mem_addr, buf, mov_size);
+  //       file_off += mov_size;
+  //       mem_addr += mov_size;
+  //       len += mov_size;
+  //     }
+  //     memset(mem_addr, 0, elf_phentry.p_memsz - len);
+  //   }
+  // }
+  // fs_close(fd);
+  // return elf_head.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename)
