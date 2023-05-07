6343f18c3a7045073766252bebbe11434c58a76e
tracer-ics2019 <tracer@njuics.org>
2023-05-07 00:25:56 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:25:56 up 18:41,  0 users,  load average: 0.46, 0.25, 0.10 988325bae3c3fb3a3ef94be449fd37bd4fd29f1

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "syscall.h"
+#include "fs.h"
 
 _Context *do_syscall(_Context *c)
 {
@@ -21,17 +22,30 @@ _Context *do_syscall(_Context *c)
     break;
   case SYS_write:
   {
-    int fd = a[1];
-    char *buf = (char *)a[2];
-    if (fd != 1 && fd != 2)
-      panic("not impletement");
-    for (int i = 0; i < a[3]; i++)
-      _putc(buf[i]);
-    c->GPRx = a[3];
+    // int fd = a[1];
+    // char *buf = (char *)a[2];
+    // if (fd != 1 && fd != 2)
+    //   panic("not impletement");
+    // for (int i = 0; i < a[3]; i++)
+    //   _putc(buf[i]);
+    // c->GPRx = a[3];
+    c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
   }
   break;
   case SYS_brk:
     c->GPRx = 0;
+    break;
+  case SYS_open:
+    c->GPRx = fs_open((void *)a[1], a[2], a[3]);
+    break;
+  case SYS_read:
+    c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
+    break;
+  case SYS_lseek:
+    c->GPRx = fs_lseek(a[1], a[2], a[3]);
+    break;
+  case SYS_close:
+    c->GPRx = fs_close(a[1]);
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
