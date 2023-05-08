fff2df580793b29696b1bf1383c707a81930e39d
tracer-ics2019 <tracer@njuics.org>
2023-05-08 03:48:48 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:48:48 up 20 min,  1 user,  load average: 0.45, 0.41, 0.26 cb29d6797f2856f7ad338e31727b0f2bc1b9852a

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -4,7 +4,7 @@
 #include <ndl.h>
 
 int main() {
-  // NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
+  NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
   // NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
   // assert(bmp->pixels);
   // printf("%d %d", bmp->w, bmp->h);
