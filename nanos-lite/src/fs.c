de923df379122eee7d50b5e45d5eb745b0ed4ae4
tracer-ics2019 <tracer@njuics.org>
2023-05-07 23:16:41 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:16:41 up 19 min,  0 users,  load average: 0.24, 0.17, 0.12 9c7f2eccbcad01013eb996b2d66a2e939574b6dc

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -69,6 +69,8 @@ int fs_open(const char *pathname, int flags, int mode)
 
 size_t fs_read(int fd, void *buf, size_t len)
 {
+  if (file_table[fd].read != NULL)
+    return file_table[fd].read(buf, 0, len);
   int paddr = file_table[fd].disk_offset + file_table[fd].open_offset;
   int end = file_table[fd].disk_offset + file_table[fd].size;
   size_t real_len = ((paddr + len <= end) ? len : (end - paddr));
