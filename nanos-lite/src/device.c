a664da76faa480af07ed66eea37a264164a42876
tracer-ics2019 <tracer@njuics.org>
2023-05-08 00:54:51 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:54:51 up 3 min,  0 users,  load average: 0.19, 0.11, 0.04 36789f9eb90103ac533d28257473429ad40ee6ad

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,7 +46,7 @@ size_t events_read(void *buf, size_t offset, size_t len)
   //   return read_size;
   // }
   ((char *)buf)[0] = '\n';
-  return 0;
+  return 1;
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
