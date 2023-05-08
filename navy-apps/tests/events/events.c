ff98798337fb14cbba5ea166ed034a39c57a9afe
tracer-ics2019 <tracer@njuics.org>
2023-05-08 00:57:29 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:57:29 up 6 min,  1 user,  load average: 0.68, 0.55, 0.23 7fc5e63b0742d10a3e71b26605e9d5add46e992

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -23,6 +23,9 @@ int main() {
     else if (time % 1024 == 0) {
       printf("receive time event for the %dth time: %s", time, buf);
     }
+    int num = 0;
+    while (num < 100000000) 
+      num++;
   }
 
   fclose(fp);
