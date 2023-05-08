950ad28a90998f5751090f994ae5c85441ed7b3b
tracer-ics2019 <tracer@njuics.org>
2023-05-08 03:56:02 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:56:02 up 27 min,  1 user,  load average: 0.18, 0.25, 0.25 343afb246b4e26bcf6364563efd217b9671b5f3

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -42,14 +42,13 @@ extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0,0, invalid_read, invalid_write},
-  {"stdout", 0, 0,0, invalid_read, serial_write},
-  {"stderr", 0, 0,0, invalid_read, serial_write},
-  {"/dev/events", 0, 0,0, events_read, invalid_write},
-  {"/dev/fb",0,0,0,invalid_read,fb_write},
-  {"/dev/fbsync",0,0,0,invalid_read,fbsync_write},
-  {"/proc/dispinfo",128,0,0,dispinfo_read,invalid_write},
-  {"/dev/tty",0,0,0,invalid_read,serial_write},
+    {"stdin", 0, 0, 0, invalid_read, invalid_write},
+    {"stdout", 0, 0, 0, invalid_read, serial_write},
+    {"stderr", 0, 0, 0, invalid_read, serial_write},
+    {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
+    {"/dev/events", 0, 0, 0, events_read, invalid_write},
+    {"/dev/fbsync", 0, 0, 0, invalid_read, fbsync_write},
+    {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
