7bb20f22cdaa875cd24bb2ba9e812ba866e891b1
tracer-ics2019 <tracer@njuics.org>
2023-05-08 03:49:12 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:49:12 up 20 min,  1 user,  load average: 0.39, 0.40, 0.26 b9cf59557f0b7ca42bb9111774f8762459993bb0

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -8,7 +8,7 @@ int main() {
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
   assert(bmp->pixels);
   printf("%d %d", bmp->w, bmp->h);
-  // NDL_OpenDisplay(bmp->w, bmp->h);
+  NDL_OpenDisplay(bmp->w, bmp->h);
   // NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
   // NDL_Render();
   // NDL_CloseDisplay();
