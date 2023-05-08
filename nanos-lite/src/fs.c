7ee1f88f84f715101f8bbef4bc991bf691f24055
tracer-ics2019 <tracer@njuics.org>
2023-05-08 04:08:05 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  04:08:05 up 39 min,  1 user,  load average: 0.50, 0.35, 0.29 43369dc5ebe00c2fa45fbad9b420cce6e51fbac8

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -104,7 +104,7 @@ size_t fs_read(int fd, void *buf, size_t len)
   if (file_table[fd].write != NULL)
   {
     size_t res = file_table[fd].read(buf, file_table[fd].open_offset, len);
-    file_table[fd].open_offset += len;
+    file_table[fd].open_offset += res;
     return res;
   }
   int paddr = file_table[fd].disk_offset + file_table[fd].open_offset;
@@ -121,7 +121,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
   if (file_table[fd].write != NULL)
   {
     size_t res = file_table[fd].write(buf, file_table[fd].open_offset, len);
-    file_table[fd].open_offset += len;
+    file_table[fd].open_offset += res;
     return res;
   }
   int paddr = file_table[fd].disk_offset + file_table[fd].open_offset;
