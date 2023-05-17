5ffe0ea9f3596e00106be661d68884ad74058765
tracer-ics2019 <tracer@njuics.org>
2023-05-16 18:34:01 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:34:01 up 1 min,  0 users,  load average: 0.99, 0.36, 0.13 4635acfccc206cbff429ad507881176cabd967ed

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc()
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/init");
+  naive_uload(NULL, "/bin/hello");
 }
 
 _Context *schedule(_Context *prev)
