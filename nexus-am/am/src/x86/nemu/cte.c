99b76d6c75e0506e93a9469fb7a41f9d2257e65e
tracer-ics2019 <tracer@njuics.org>
2023-05-20 07:11:12 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:11:12 up  4:32,  0 users,  load average: 0.00, 0.07, 0.13 b4b8125f1bbd42bb424d07f5de9125ec85cc54e8

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -65,7 +65,7 @@ _Context *_kcontext(_Area stack, void (*entry)(void *), void *arg)
 {
   void **arg_stack = stack.end - sizeof(void*);
   *arg_stack = arg;
-  _Context* new_context = stack.end - sizeof(_Context) - sizeof(void*);
+  _Context* new_context = stack.end - sizeof(_Context) - sizeof(void*) * 2;
   new_context->esp = (uintptr_t)(&new_context->irq);
   new_context->eip = (uintptr_t)entry;
   new_context->cs = 8;
