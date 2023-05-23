5414792df53f6b6ec3823fa51ed8b3ce27ebfb2a
tracer-ics2019 <tracer@njuics.org>
2023-05-23 01:07:50 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:07:50 up  5:36,  1 user,  load average: 0.50, 0.56, 0.35 aacbe089b1cb628dc05ae0e94247a43ee2661b1c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -17,67 +17,6 @@ extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 #define ROUNDUP(a, sz) ((((uintptr_t)a) + (sz)-1) & ~((sz)-1))
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
-  // TODO();
-  // int fd=fs_open(filename, 0, 0);//f_offset=get_disk_offset(fd),seg_size;
-  // Elf_Phdr head_pro;
-  // Elf_Ehdr head_elf;
-  // fs_read(fd,&head_elf,sizeof(Elf_Ehdr));
-  // void *pa;//*va=(void*)head_elf.e_entry;
-  // size_t siz=head_elf.e_phentsize,cnt=head_elf.e_phnum;
-  // uintptr_t now_v,v_addr;
-  // size_t page_num;
-  // for (int i=0;i<cnt;++i)
-  // {
-  // 	fs_lseek(fd,i*siz+head_elf.e_phoff,0);
-  // 	fs_read(fd,&head_pro,siz);
-  // 	if (head_pro.p_type!=PT_LOAD) continue;
-  // 	fs_lseek(fd,head_pro.p_offset,0);
-  // 	v_addr=head_pro.p_vaddr;
-  // 	//if ((head_pro.p_vaddr&0xfff)) assert(0);
-  // 	page_num=(head_pro.p_filesz-1)/PGSIZE+1;//申请的页数
-  // 	for (int j=0;j<page_num;++j)
-  // 	{
-  // 		pa=new_page(1);
-  // 		if ((uintptr_t)pa&0xfff) assert(0);
-  // 		_map(&(pcb->as),(void*)(head_pro.p_vaddr+j*PGSIZE),pa,0);
-  // 		//fs_read(fd,(void*)(head_pro.p_vaddr),head_pro.p_filesz);
-  // 		if (j<page_num-1) fs_read(fd,pa,PGSIZE);
-  // 		else fs_read(fd,pa,head_pro.p_filesz-PGSIZE*j);
-  // 	}
-  // 	//TODO 是否要清空？
-  // 	//maxn=(maxn>head_pro.p_memsz+head_pro.p_memsz?head_pro.p_memsz+head_pro.p_vaddr:maxn);
-  // 	//if (maxn&0xfff) maxn=((maxn+0xfff)&0xfffff000);
-  // 	//continue;
-  // 	v_addr+=page_num*PGSIZE;
-  // 	if (head_pro.p_filesz==head_pro.p_memsz) {pcb->max_brk=v_addr;continue;}
-
-  // 	int zero_len=head_pro.p_memsz-head_pro.p_filesz;
-  // 	if (zero_len<PGSIZE*page_num-head_pro.p_filesz)
-  // 		memset((void*)(((uintptr_t)pa)+(head_pro.p_filesz-PGSIZE*(page_num-1))),0,zero_len);
-  // 	else
-  // 	{
-  // 		memset((void*)(((uintptr_t)pa)+(head_pro.p_filesz-PGSIZE*(page_num-1))),0,PGSIZE*page_num-head_pro.p_filesz);
-  // 		zero_len-=(PGSIZE*page_num-head_pro.p_filesz);
-  // 		now_v=head_pro.p_vaddr+page_num*PGSIZE;
-  // 		page_num=(zero_len-1)/PGSIZE+1;
-  // 		for (int j=0;j<page_num;++j)
-  // 		{
-  // 			pa=new_page(1);
-  // 			_map(&(pcb->as),(void*)(now_v),pa,0);
-  // 			if ((uintptr_t)pa&0xfff) assert(0);
-  // 			if (j<page_num-1) memset(pa,0,PGSIZE);
-  // 			else memset(pa,0,zero_len);
-  // 			now_v+=PGSIZE;
-  // 			zero_len-=PGSIZE;
-  // 		}
-  // 		v_addr+=page_num*PGSIZE;
-  // 	}
-  // 	pcb->max_brk=v_addr;
-  // 	//memset((void*)(((uintptr_t)pa)+head_pro.p_filesz),0,head_pro.p_memsz-head_pro.p_filesz);
-  // }
-  // fs_close(fd);
-  // //current->max_brk=maxn;
-  // return head_elf.e_entry;
   const int page_size = 4096;
   int fd = fs_open(filename, 0, 0);
   Elf_Ehdr elf_head;
@@ -127,6 +66,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
         v_addr += mov_size;
         len += mov_size;
       }
+      // max_brk应该是最后一个段吧
       pcb->max_brk = pcb->max_brk > ROUNDUP(v_addr, page_size) ? pcb->max_brk : ROUNDUP(v_addr, page_size);
     }
   }
