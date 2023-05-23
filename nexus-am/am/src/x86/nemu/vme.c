56d16b32fcf77106d38993843aa00e196ad465b4
tracer-ics2019 <tracer@njuics.org>
2023-05-22 19:55:42 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:55:42 up 24 min,  1 user,  load average: 0.17, 0.13, 0.15 6f9fdc5536406722669f47a4a7383fe5ff79df81

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -51,9 +51,9 @@ int _vme_init(void *(*pgalloc_f)(size_t), void (*pgfree_f)(void *))
   }
 
   set_cr3(kpdirs);
-  // set_cr0(get_cr0() | CR0_PG);
+  _putc('1');
+  set_cr0(get_cr0() | CR0_PG);
   vme_enable = 1;
-
 
   return 0;
 }
