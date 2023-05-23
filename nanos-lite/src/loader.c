284d020e45175d6dced1cb6b38f37b0fb56b400e
tracer-ics2019 <tracer@njuics.org>
2023-05-22 21:55:28 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:55:28 up  2:24,  1 user,  load average: 0.23, 0.11, 0.03 8b79c6029e8cae015ce63976b101b99ce390f090

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -32,10 +32,10 @@ static uintptr_t loader(PCB *pcb, const char *filename)
       size_t file_off = elf_phentry.p_offset;
       fs_lseek(fd, file_off, SEEK_SET);
       unsigned char *v_addr = (unsigned char *)elf_phentry.p_vaddr;
-      while (len < elf_phentry.p_memsz)
+      while (len < elf_phentry.p_filesz)
       {
-        int mov_size = (elf_phentry.p_memsz - len > page_size ? page_size
-                                                              : elf_phentry.p_memsz - len);
+        int mov_size = (elf_phentry.p_filesz - len > page_size ? page_size
+                                                               : elf_phentry.p_filesz - len);
         int gap = page_size - ((uint32_t)v_addr & 0xfff);
         if (mov_size > gap)
           mov_size = gap;
@@ -43,10 +43,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
         void *p_addr = new_page(1);
         p_addr = (void *)((uint32_t)p_addr | ((uint32_t)v_addr & 0xfff));
         _map(&pcb->as, v_addr, p_addr, _PROT_EXEC);
-        if (len <= elf_phentry.p_filesz)
-          fs_read(fd, p_addr, mov_size);
-        else
-          memset(p_addr, 0, mov_size);
+        fs_read(fd, p_addr, mov_size);
         v_addr += mov_size;
         len += mov_size;
       }
