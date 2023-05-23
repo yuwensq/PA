83c0cf20edb293418daa3c09756f43203fdff60b
tracer-ics2019 <tracer@njuics.org>
2023-05-23 00:51:14 -0700

    >  compile 2010234 yuwensq Linux ubuntu 5.4.0-148-generic #165~18.04.1-Ubuntu SMP Thu Apr 20 01:14:06 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:51:14 up  5:20,  1 user,  load average: 0.07, 0.10, 0.03 db16924bcd1e2dc1ca3a089dbc2c0758f1619f1a

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -42,7 +42,7 @@ make_group(gp4,
 /* 0xff */
 make_group(gp5,
     EX(inc), EX(dec), EX(call_rm), EMPTY,
-    EX(jmp_rm), EMPTY, EX(push), EMPTY)
+    EX(jmp_rm), EMPTY, EX(push), EX(nop))
 
 /* 0x0f 0x01*/
 make_group(gp7,
