fc03baecb5f3fae0215cc5e2d41b4b2fa65a38e1
tracer-ics2019 <tracer@njuics.org>
2023-05-28 20:01:45 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-149-generic #166~18.04.1-Ubuntu SMP Fri Apr 21 16:42:44 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:01:45 up 12 min,  1 user,  load average: 0.64, 0.40, 0.25 e5f56e96f4e7cc93e9ce85df52a8ade3314c49b0

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -52,7 +52,7 @@ _Context *schedule(_Context *prev)
   current->cp = prev;
   // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   current = front_p;
-  if (pcb1_times == 2) {
+  if (pcb1_times == 50) {
     current = &pcb[3];
     pcb1_times = 0;
   }
