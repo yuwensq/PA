9e1601c8ec694a1bd7f84476841d3f93735179fb
tracer-ics2019 <tracer@njuics.org>
2023-05-28 20:01:25 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-149-generic #166~18.04.1-Ubuntu SMP Fri Apr 21 16:42:44 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:01:25 up 12 min,  1 user,  load average: 0.52, 0.36, 0.23 6942f7f6852a44c06ac36d9bbe929cf8e34cfef

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,9 +33,9 @@ void init_proc()
 {
   // context_kload(&pcb[0], hello_fun, "kernel thread 1");
   context_uload(&pcb[0], "/bin/pal", 1, arg, NULL);
-  context_uload(&pcb[1], "/bin/events", 1, arg, NULL);
-  context_uload(&pcb[2], "/bin/pal", 1, arg, NULL);
-  context_uload(&pcb[3], "/bin/pal", 1, arg, NULL);
+  // context_uload(&pcb[1], "/bin/pal", 1, arg, NULL);
+  // context_uload(&pcb[2], "/bin/pal", 1, arg, NULL);
+  context_uload(&pcb[3], "/bin/events", 1, arg, NULL);
   // context_kload(&pcb[1], hello_fun, "kernel thread 2");
   front_p = &pcb[0];
   switch_boot_pcb();
@@ -53,7 +53,7 @@ _Context *schedule(_Context *prev)
   // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   current = front_p;
   if (pcb1_times == 2) {
-    current = &pcb[1];
+    current = &pcb[3];
     pcb1_times = 0;
   }
   pcb1_times ++;
