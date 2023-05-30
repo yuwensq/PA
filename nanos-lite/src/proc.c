0a70e630478962696602f3e24815c649845924ad
tracer-ics2019 <tracer@njuics.org>
2023-05-30 03:17:30 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-149-generic #166~18.04.1-Ubuntu SMP Fri Apr 21 16:42:44 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:17:30 up  5:26,  1 user,  load average: 0.26, 0.47, 0.71 f59783aac9b874d36c9c6269e77375ec21db55a

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,7 @@ char *arg[] = {"/bin/pal", "--skip"};
 void init_proc()
 {
   context_kload(&pcb[0], hello_fun, "kernel thread 1");
-  context_kload(&pcb[3], hello_fun, "kernel thread 2");
+  // context_kload(&pcb[3], hello_fun, "kernel thread 2");
 
   // context_uload(&pcb[0], "/bin/pal", 2, arg, NULL);
   // context_uload(&pcb[1], "/bin/pal", 2, arg, NULL);
