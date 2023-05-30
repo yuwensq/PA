26032e3b4472a4870cb759800405d429ca7164d2
tracer-ics2019 <tracer@njuics.org>
2023-05-30 03:29:20 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-149-generic #166~18.04.1-Ubuntu SMP Fri Apr 21 16:42:44 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:29:20 up  5:38,  1 user,  load average: 0.45, 0.21, 0.39 d2c0d8a3e41d8207f27b51dcbf699c684bd2c16d

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,7 @@ char *arg[] = {"/bin/pal", "--skip"};
 void init_proc()
 {
   context_kload(&pcb[0], hello_fun, "kernel thread 1");
-  // context_kload(&pcb[3], hello_fun, "kernel thread 2");
+  context_kload(&pcb[3], hello_fun, "kernel thread 2");
 
   // context_uload(&pcb[0], "/bin/pal", 2, arg, NULL);
   // context_uload(&pcb[1], "/bin/pal", 2, arg, NULL);
@@ -56,7 +56,7 @@ _Context *schedule(_Context *prev)
   current = front_p;
   if (pcb1_times == 50)
   {
-    current = &pcb[0];
+    current = &pcb[3];
     pcb1_times = 0;
   }
   pcb1_times++;
