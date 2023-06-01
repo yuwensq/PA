a51c20a9692349c3198b22115e2ff8a824b98cd9
tracer-ics2019 <tracer@njuics.org>
2023-06-01 06:45:24 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-149-generic #166~18.04.1-Ubuntu SMP Fri Apr 21 16:42:44 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:45:24 up 10:10,  1 user,  load average: 0.53, 0.13, 0.04 e71b05c4b940c1753e37154b517a0e47f33ac7f

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -18,7 +18,7 @@ static _Context *do_event(_Event e, _Context *c)
     do_syscall(c);
     break;
   case _EVENT_IRQ_TIMER:
-    Log("timer irq");
+    // Log("timer irq");
     _yield();
     break;
   default:
