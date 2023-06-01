c4b6a84efb9e65d24c833e6f616821d196e16c04
tracer-ics2019 <tracer@njuics.org>
2023-05-31 21:31:53 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-149-generic #166~18.04.1-Ubuntu SMP Fri Apr 21 16:42:44 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:31:53 up  2:29,  1 user,  load average: 1.09, 1.04, 1.01 2cfa812482385c249e762e0032cd1855341736bd

--- navy-apps/apps/pal/src/pal-main.c
+++ navy-apps/apps/pal/src/pal-main.c
@@ -3,14 +3,13 @@
 void main_loop();
 void hal_init();
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	Log("game start!");
 	for (int i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-	
 
-  hal_init();
+	hal_init();
 	main_loop();
 
 	return 0;
