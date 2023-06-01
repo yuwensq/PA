8a60cae90d7c2ddea9d54773f5bea58dbd25fc2e
tracer-ics2019 <tracer@njuics.org>
2023-05-31 20:01:00 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-149-generic #166~18.04.1-Ubuntu SMP Fri Apr 21 16:42:44 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:01:00 up 58 min,  1 user,  load average: 1.48, 0.63, 0.31 9062b9bd280085fcf60591698f60fbf61db1523

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -18,7 +18,7 @@ void hello_fun(void *arg)
   while (1)
   {
     // Log("Hello World from Nanos-lite for the %dth time! from: %s", j, (char *)arg);
-    Log("Hello World from Nanos-lite for the %dth time!");
+    Log("Hello World from Nanos-lite for the %dth time!", j);
     j++;
     _yield();
   }
