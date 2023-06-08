3ab19464f0be81bf3ce61d65509dd3adafe7722b
tracer-ics2019 <tracer@njuics.org>
2023-06-08 02:11:25 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:11:25 up  6:11,  1 user,  load average: 0.32, 0.46, 0.53 bdfdece788c803c6a7f8818038d45cfb92c6477

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -39,8 +39,7 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 //   return res;
 // }
 
-FLOAT f2F(float a)
-{
+FLOAT f2F(float a) {
   /* You should figure out how to convert `a' into FLOAT without
    * introducing x87 floating point instructions. Else you can
    * not run this code in NEMU before implementing x87 floating
@@ -50,43 +49,84 @@ FLOAT f2F(float a)
    * stack. How do you retrieve it to another variable without
    * performing arithmetic operations on it directly?
    */
-  // assert(0);
-  const int e_min = 126; // e_min是负的
-  const int e_max = 127;
-  unsigned int bits = *(unsigned int *)(void *)(&a);
-  unsigned int s = ((bits & 0x80000000) >> 31);
-  unsigned int e = ((bits & 0x7f800000) >> 23);
-  unsigned int m = (bits & 0x007fffff);
-  FLOAT res = 0;
-  if (e == 0)
-  {
-    // 这里，只要e==0，就算m不等于零，用FLOAT也表示不了那么小的数，直接给零吧
-    // if (m == 0)
-    //   res = 0;
-    // else {
-    //   res = 0;
-    // }
-    res = 0;
-  }
-  else if (e == 128)
-  { // 要不是无穷，要不是nan，先不处理吧
+
+  //assert(0);
+  //return 0;
+  union float_ {
+    struct {
+      uint32_t man : 23;
+      uint32_t exp : 8;
+      uint32_t sign : 1;
+    };
+    uint32_t val;
+  };
+  union float_ f;
+  f.val = *((uint32_t*)(void*)&a);
+  int exp = f.exp - 127;
+  FLOAT ret = 0;
+  if (exp == 128)
     assert(0);
+  if (exp >= 0) {
+    int mov = 7 - exp;
+    if (mov >= 0)
+      ret = (f.man | (1 << 23)) >> mov;
+    else
+      ret = (f.man | (1 << 23)) << (-mov);
   }
   else
-  {
-    m = (0x00800000 | m);
-    m >>= 7;
-    if (e >= 127)
-      m <<= (e - 127);
-    else
-      m >>= (127 - e);
-    if (s == 1)
-      res = -((int)m);
-    else
-      res = m;
-  }
-  return res;
+    return 0;
+  return f.sign == 0 ? ret : -ret;
 }
+
+
+// FLOAT f2F(float a)
+// {
+//   /* You should figure out how to convert `a' into FLOAT without
+//    * introducing x87 floating point instructions. Else you can
+//    * not run this code in NEMU before implementing x87 floating
+//    * point instructions, which is contrary to our expectation.
+//    *
+//    * Hint: The bit representation of `a' is already on the
+//    * stack. How do you retrieve it to another variable without
+//    * performing arithmetic operations on it directly?
+//    */
+//   // assert(0);
+//   const int e_min = 126; // e_min是负的
+//   const int e_max = 127;
+//   unsigned int bits = *(unsigned int *)(void *)(&a);
+//   unsigned int s = ((bits & 0x80000000) >> 31);
+//   unsigned int e = ((bits & 0x7f800000) >> 23);
+//   unsigned int m = (bits & 0x007fffff);
+//   FLOAT res = 0;
+//   if (e == 0)
+//   {
+//     // 这里，只要e==0，就算m不等于零，用FLOAT也表示不了那么小的数，直接给零吧
+//     // if (m == 0)
+//     //   res = 0;
+//     // else {
+//     //   res = 0;
+//     // }
+//     res = 0;
+//   }
+//   else if (e == 128)
+//   { // 要不是无穷，要不是nan，先不处理吧
+//     assert(0);
+//   }
+//   else
+//   {
+//     m = (0x00800000 | m);
+//     m >>= 7;
+//     if (e >= 127)
+//       m <<= (e - 127);
+//     else
+//       m >>= (127 - e);
+//     if (s == 1)
+//       res = -((int)m);
+//     else
+//       res = m;
+//   }
+//   return res;
+// }
 
 FLOAT Fabs(FLOAT a)
 {
