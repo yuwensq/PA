4123e73f7ef7d3986f5d018cba1bc6ef98c17837
tracer-ics2019 <tracer@njuics.org>
2023-06-08 02:48:20 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:48:20 up  6:48,  1 user,  load average: 0.30, 0.08, 0.18 f147faa4d8f10405633ef49eed997e71cc18a6aa

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -9,82 +9,21 @@ FLOAT F_mul_F(FLOAT a, FLOAT b)
   return res;
 }
 
-// FLOAT F_div_F(FLOAT a, FLOAT b)
-// {
-//   // assert(0);
-//   // return 0;
-//   assert(b != 0);
-//   FLOAT x = Fabs(a);
-//   FLOAT y = Fabs(b);
-//   FLOAT ret = x / y;
-//   x = x % y;
-
-//   for (int i = 0; i < 16; i++)
-//   {
-//     x <<= 1;
-//     ret <<= 1;
-//     if (x >= y)
-//     {
-//       x -= y;
-//       ret++;
-//     }
-//   }
-//   if (((a ^ b) & 0x80000000) == 0x80000000)
-//   {
-//     ret = -ret;
-//   }
-//   return ret;
-// }
-
 FLOAT F_div_F(FLOAT a, FLOAT b)
 {
   // assert(0);
-  FLOAT res = ((a / b) << 16);
+  int is_neg = ((a < 0) != (b < 0));
+  // 这里如果简单的让res = ((a / b) << 16)，会丢失小数部分
+  FLOAT res = a / b;
+  int rem = a % b;
+  rem <<= 16;
+  rem /= b;
+  res <<= 16;
+  res |= rem;
+  if (is_neg)
+    res = -res;
   return res;
 }
-
-// FLOAT f2F(float a)
-// {
-//   /* You should figure out how to convert `a' into FLOAT without
-//    * introducing x87 floating point instructions. Else you can
-//    * not run this code in NEMU before implementing x87 floating
-//    * point instructions, which is contrary to our expectation.
-//    *
-//    * Hint: The bit representation of `a' is already on the
-//    * stack. How do you retrieve it to another variable without
-//    * performing arithmetic operations on it directly?
-//    */
-
-//   // assert(0);
-//   // return 0;
-//   union float_
-//   {
-//     struct
-//     {
-//       uint32_t man : 23;
-//       uint32_t exp : 8;
-//       uint32_t sign : 1;
-//     };
-//     uint32_t val;
-//   };
-//   union float_ f;
-//   f.val = *((uint32_t *)(void *)&a);
-//   int exp = f.exp - 127;
-//   FLOAT ret = 0;
-//   if (exp == 128)
-//     assert(0);
-//   if (exp >= 0)
-//   {
-//     int mov = 7 - exp;
-//     if (mov >= 0)
-//       ret = (f.man | (1 << 23)) >> mov;
-//     else
-//       ret = (f.man | (1 << 23)) << (-mov);
-//   }
-//   else
-//     return 0;
-//   return f.sign == 0 ? ret : -ret;
-// }
 
 FLOAT f2F(float a)
 {
@@ -98,8 +37,6 @@ FLOAT f2F(float a)
    * performing arithmetic operations on it directly?
    */
   // assert(0);
-  const int e_min = 126; // e_min是负的
-  const int e_max = 127;
   unsigned int bits = *(unsigned int *)(void *)(&a);
   unsigned int s = ((bits & 0x80000000) >> 31);
   unsigned int e = ((bits & 0x7f800000) >> 23);
