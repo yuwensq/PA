eed16ebcdd423adc8b9c5dff2491b7421068e6b2
tracer-ics2019 <tracer@njuics.org>
2023-06-08 02:10:19 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:10:19 up  6:10,  1 user,  load average: 0.20, 0.45, 0.52 825b7d1cfb665f96e584555fad41aa4d69d197d1

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -39,33 +39,6 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 //   return res;
 // }
 
-// FLOAT F_div_F(FLOAT a, FLOAT b)
-// {
-//   // assert(0);
-//   // return 0;
-//   assert(b != 0);
-//   FLOAT x = Fabs(a);
-//   FLOAT y = Fabs(b);
-//   FLOAT z = x / y;
-//   x = x % y;
-
-//   for (int i = 0; i < 16; i++)
-//   {
-//     x <<= 1;
-//     z <<= 1;
-//     if (x >= y)
-//     {
-//       x -= y;
-//       z++;
-//     }
-//   }
-//   if (((a ^ b) & 0x80000000) == 0x80000000)
-//   {
-//     z = -z;
-//   }
-//   return z;
-// }
-
 FLOAT f2F(float a)
 {
   /* You should figure out how to convert `a' into FLOAT without
@@ -77,65 +50,42 @@ FLOAT f2F(float a)
    * stack. How do you retrieve it to another variable without
    * performing arithmetic operations on it directly?
    */
-  struct float_ *f = (struct float_ *)&a;
-  uint32_t res;
-  uint32_t frac;
-  int exp;
-  if ((f->exp & 0xff) == 0xff)
-    assert(0);
-  else if (f->exp == 0)
+  // assert(0);
+  const int e_min = 126; // e_min是负的
+  const int e_max = 127;
+  unsigned int bits = *(unsigned int *)(void *)(&a);
+  unsigned int s = ((bits & 0x80000000) >> 31);
+  unsigned int e = ((bits & 0x7f800000) >> 23);
+  unsigned int m = (bits & 0x007fffff);
+  FLOAT res = 0;
+  if (e == 0)
   {
-    exp = 1 - 127;
-    frac = (f->frac & 0x7fffff);
+    // 这里，只要e==0，就算m不等于零，用FLOAT也表示不了那么小的数，直接给零吧
+    // if (m == 0)
+    //   res = 0;
+    // else {
+    //   res = 0;
+    // }
+    res = 0;
+  }
+  else if (e == 128)
+  { // 要不是无穷，要不是nan，先不处理吧
+    assert(0);
   }
   else
   {
-    exp = f->exp - 127;
-    frac = (f->frac & 0x7fffff) | (1 << 23);
+    m = (0x00800000 | m);
+    m >>= 7;
+    if (e >= 127)
+      m <<= (e - 127);
+    else
+      m >>= (127 - e);
+    if (s == 1)
+      res = -((int)m);
+    else
+      res = m;
   }
-  if (exp >= 7 && exp < 22)
-    res = frac << (exp - 7);
-  else if (exp < 7 && exp > -32)
-    res = frac >> 7 >> -exp;
-  else
-    assert(0);
-  return (f->sign) ? -res : res;
-  // // assert(0);
-  // const int e_min = 126; // e_min是负的
-  // const int e_max = 127;
-  // unsigned int bits = *(unsigned int *)(void *)(&a);
-  // unsigned int s = ((bits & 0x80000000) >> 31);
-  // unsigned int e = ((bits & 0x7f800000) >> 23);
-  // unsigned int m = (bits & 0x007fffff);
-  // FLOAT res = 0;
-  // if (e == 0)
-  // {
-  //   // 这里，只要e==0，就算m不等于零，用FLOAT也表示不了那么小的数，直接给零吧
-  //   // if (m == 0)
-  //   //   res = 0;
-  //   // else {
-  //   //   res = 0;
-  //   // }
-  //   res = 0;
-  // }
-  // else if (e == 128)
-  // { // 要不是无穷，要不是nan，先不处理吧
-  //   assert(0);
-  // }
-  // else
-  // {
-  //   m = (0x00800000 | m);
-  //   m >>= 7;
-  //   if (e >= 127)
-  //     m <<= (e - 127);
-  //   else
-  //     m >>= (127 - e);
-  //   if (s == 1)
-  //     res = -((int)m);
-  //   else
-  //     res = m;
-  // }
-  // return res;
+  return res;
 }
 
 FLOAT Fabs(FLOAT a)
