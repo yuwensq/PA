e4387285b4d4468d5ce084730f38ab6c822cd265
tracer-ics2019 <tracer@njuics.org>
2023-06-08 02:12:30 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:12:30 up  6:12,  1 user,  load average: 0.69, 0.55, 0.55 65d89fdfbf957d4fdba44e373a7a49cbda85bda3

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -1,13 +1,17 @@
 #include "FLOAT.h"
 #include <stdint.h>
 #include <assert.h>
-
-FLOAT F_mul_F(FLOAT a, FLOAT b)
-{
-  // assert(0);
-  FLOAT res = ((a * b) >> 16);
-  return res;
+FLOAT F_mul_F(FLOAT a, FLOAT b) {
+  //assert(0);
+  //return 0;
+  return ((int64_t)a * (int64_t)b) >> 16;
 }
+// FLOAT F_mul_F(FLOAT a, FLOAT b)
+// {
+//   // assert(0);
+//   FLOAT res = ((a * b) >> 16);
+//   return res;
+// }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
   //assert(0);
