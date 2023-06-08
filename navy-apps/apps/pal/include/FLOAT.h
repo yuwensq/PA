59e1d80dd7e08a4c24f1a75459dfb38a93fba4e2
tracer-ics2019 <tracer@njuics.org>
2023-06-08 01:13:19 -0700

    >  run 2010234 yuwensq Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:13:19 up  5:13,  1 user,  load average: 0.95, 0.66, 0.81 71d6e98df56c79591fe157a663198949e1c3cc6b

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -5,24 +5,33 @@
 
 typedef int FLOAT;
 
-static inline int F2int(FLOAT a) {
-  assert(0);
-  return 0;
+static inline int F2int(FLOAT a)
+{
+  // assert(0);
+  // 这里因为FLOAT是int，右移是算数右移，所以没毛病
+  int res = (a >> 16);
+  return res;
 }
 
-static inline FLOAT int2F(int a) {
-  assert(0);
-  return 0;
+static inline FLOAT int2F(int a)
+{
+  // assert(0);
+  FLOAT res = (a << 16);
+  return res;
 }
 
-static inline FLOAT F_mul_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+static inline FLOAT F_mul_int(FLOAT a, int b)
+{
+  // assert(0);
+  FLOAT res = (a * b);
+  return res;
 }
 
-static inline FLOAT F_div_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+static inline FLOAT F_div_int(FLOAT a, int b)
+{
+  // assert(0);
+  FLOAT res = (a / b);
+  return res;
 }
 
 FLOAT f2F(float);
--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -2,17 +2,22 @@
 #include <stdint.h>
 #include <assert.h>
 
-FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+FLOAT F_mul_F(FLOAT a, FLOAT b)
+{
+  // assert(0);
+  FLOAT res = ((a * b) >> 16);
+  return res;
 }
 
-FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+FLOAT F_div_F(FLOAT a, FLOAT b)
+{
+  // assert(0);
+  FLOAT res = ((a / b) << 16);
+  return res;
 }
 
-FLOAT f2F(float a) {
+FLOAT f2F(float a)
+{
   /* You should figure out how to convert `a' into FLOAT without
    * introducing x87 floating point instructions. Else you can
    * not run this code in NEMU before implementing x87 floating
@@ -22,38 +27,67 @@ FLOAT f2F(float a) {
    * stack. How do you retrieve it to another variable without
    * performing arithmetic operations on it directly?
    */
-
+  // 这里要不先不考虑无穷和NAN？
   assert(0);
+  const int e_min = 126; // e_min是负的
+  const int e_max = 127;
+  unsigned int bits = *(unsigned int *)(void *)(&a);
+  unsigned int s = ((bits & 0x80000000) >> 31);
+  unsigned int e = ((bits & 0x7f800000) >> 23);
+  unsigned int m = (bits & 0x007fffff);
+  FLOAT res = 0;
+  if (e == 0)
+  {
+    if (m == 0)
+      res = 0;
+    else {
+      res = (m >> e_min)
+    }
+  }
+  else if (e == 128) {
+    assert(0);
+  }
+  else {
+
+  }
   return 0;
 }
 
-FLOAT Fabs(FLOAT a) {
-  assert(0);
-  return 0;
+FLOAT Fabs(FLOAT a)
+{
+  // assert(0);
+  FLOAT res = a;
+  if (a & 0x80000000)
+    res = -a;
+  return res;
 }
 
 /* Functions below are already implemented */
 
-FLOAT Fsqrt(FLOAT x) {
+FLOAT Fsqrt(FLOAT x)
+{
   FLOAT dt, t = int2F(2);
 
-  do {
+  do
+  {
     dt = F_div_int((F_div_F(x, t) - t), 2);
     t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+  } while (Fabs(dt) > f2F(1e-4));
 
   return t;
 }
 
-FLOAT Fpow(FLOAT x, FLOAT y) {
+FLOAT Fpow(FLOAT x, FLOAT y)
+{
   /* we only compute x^0.333 */
   FLOAT t2, dt, t = int2F(2);
 
-  do {
+  do
+  {
     t2 = F_mul_F(t, t);
     dt = (F_div_F(x, t2) - t) / 3;
     t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+  } while (Fabs(dt) > f2F(1e-4));
 
   return t;
 }
